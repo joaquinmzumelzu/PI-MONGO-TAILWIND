595abb418065199bd358e6e9759fc04cdcd55136
joaquinmzumelzu <joaquinmunoztrade@gmail.com>
2022-08-19 20:50:54 -0400

    ahora si

--- api/index.js
+++ api/index.js
@@ -1,14 +1,13 @@
+require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 
 const app = express()
 const router = require('./Routes/index.js')
-const PORT = 3000
-
 app.use(cors())
 app.use(express.json())
 app.use('/', router)
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`))
 
 
